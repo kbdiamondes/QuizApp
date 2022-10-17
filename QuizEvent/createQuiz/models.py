ccd13d6b2ad3720afa4845b70dec38ee363cdb01
Keith <68324799+kbdiamondes@users.noreply.github.com>
2022-10-17 17:13:33 +0800

    rainy_afternoon-fix_v.513

--- QuizEvent/createQuiz/models.py
+++ QuizEvent/createQuiz/models.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
@@ -21,33 +23,33 @@ class Student(User):
     course = models.CharField(max_length=50, null=False)
 
 
-
 class Teacher(User):
     degree = models.CharField(max_length=50, null=False)
 
 
 class Quiz(models.Model):
-    questionid = models.IntegerField(null = False, unique=True)
+    questionid = models.IntegerField(null=False)
     quizid = models.AutoField(primary_key=True)
     subjectname = models.CharField(max_length=50)
     question = models.CharField(max_length=50)
     correctanswer = models.CharField(max_length=50)
     eqpoints = models.IntegerField()
+    teacher = models.ManyToManyField(Teacher)
+
 
     def __str__(self):
-        #return 'Quiz ID:' + str(self.quizid) + ' ' + 'Question ID:' + ' ' + str(self.questionid)  + '  ' + 'Subject Name: ' + self.subjectname + '  ' + 'Question: ' + self.question
-        return 'Quiz ID:' + str(self.quizid) + ' ' + ' Question ID: ' + str(self.questionid)
+         return 'Quiz ID:' + str(self.quizid) + ' ' + 'Question ID:' + ' ' + str(self.questionid)  + '  ' + 'Subject Name: ' + self.subjectname + '  ' + 'Question: ' + self.question
+        #return 'Quiz ID:' + str(self.quizid) + ' ' + ' Question ID: ' + str(self.questionid)
 
 
 class QuizBank(models.Model):
-    quizid= models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    quizid = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     questionid = models.IntegerField()
 
 
 class StudentAnswer(models.Model):
-    #username = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='user_name')
+    student = models.ManyToManyField(Student)
     questionid = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='quiz_question')
-    quizid = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='quiz_id', primary_key=True)
     studentanswer = models.CharField(max_length=50)
 
 
@@ -55,8 +57,3 @@ class QuizResult(models.Model):
     quiz_id = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     nickname = models.ForeignKey(Student, on_delete=models.CASCADE)
     studentscore = models.IntegerField(default=0, null=False)
-
-
-
-
-
